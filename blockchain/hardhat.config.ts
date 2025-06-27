cb1e9fcb54e0cd3361929f64ee7922a582bf95d8
kjaewon4 <jaewon0002@gmail.com>
2025-06-27 22:57:43 +0900

    refactor(blockchain): 관리자 지갑 생성 방식 변경

--- blockchain/hardhat.config.ts
+++ blockchain/hardhat.config.ts
@@ -4,7 +4,13 @@ import "@typechain/hardhat";
 import "@nomicfoundation/hardhat-ethers";
 import "@nomicfoundation/hardhat-chai-matchers";
 import 'hardhat-deploy';
+import * as dotenv from "dotenv";
+dotenv.config({ path: "../backend/.env" }); // 실제 .env 경로에 맞춰주세요
 
+const ADMIN_KEY = process.env.ADMIN_PRIVATE_KEY!;
+if (!ADMIN_KEY) {
+  throw new Error("ADMIN_PRIVATE_KEY가 환경변수에 설정되어 있지 않습니다.");
+}
 const config: HardhatUserConfig = {
   solidity: {
     version: "0.8.28",
@@ -33,7 +39,18 @@ const config: HardhatUserConfig = {
   },
 
   networks: {
-    hardhat: { /* 필요하다면 옵션 추가 */ },
+     localhost: {
+      url: "http://127.0.0.1:8545",
+      // 배열 안에 문자열(privateKey)만 넣습니다.
+      accounts: [ADMIN_KEY]
+    },
+    // 내부 테스트용 네트워크
+    hardhat: {
+      accounts: [
+        // hardhat 네트워크에선 객체 형태로 잔액까지 지정 가능
+        { privateKey: ADMIN_KEY, balance: "10000000000000000000000" }
+      ]
+    }
   },
 };
 
